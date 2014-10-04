ce36d10753f2f94b144274468555535a21ff420a
criscom <criscom27@gmail.com>
2014-10-04 17:09:57 +0200

    Finished Miniproject2

--- MiniProjectWeek02.py
+++ MiniProjectWeek02.py
@@ -10,8 +10,7 @@ import math
 
 # initialize global variables
 
-num_range = 100
-# secret number for guess
+# secret number to guess
 secret_number = 0
 # number of guesses
 guesses = 0
@@ -24,6 +23,8 @@ def new_game():
     secret_number = random.randrange(0,101)
     print "New game. Range is from 0 to 100."
     print "Number of remaining guesses is 7."
+    print ""
+    #define the number of guesses
     global guesses
     guesses = 7
 
@@ -32,22 +33,59 @@ def new_game():
 # define event handlers for control panel
 def range100():
     # button that changes the range to [0,100) and starts a new game 
+    # we simply call new_game() function
     new_game()
 
 def range1000():
     # button that changes the range to [0,1000) and starts a new game     
-    global num_range
-    num_range = range(0,1001)
+    global secret_number
+    secret_number = range(0,1001)
     print "New game. Range is from 0 to 1000."
     print "Number of remaining guesses is 10."
+    print ""
+    #define the number of guesses
+    global guesses
+    guesses = 10
     
 def input_guess(guess):
-        # main game logic goes here
+    # main game logic goes here
+    #converting guess from a string to an integer
     guess = int(guess)
+    #print the guess
     print "Your guess was:", guess
+    #number of guesses, decremented with each guess
     global guesses
     guesses = guesses - 1
     print "Number of remaining guesses is", guesses
+    
+    #comparing guess with secret number
+    
+    #if guess is correct then start a new game
+    if guess == secret_number:
+        print "Correct! You are a champ!"
+        print "Let's start a new game!"
+        print ""
+        #start a new game
+        new_game()
+        
+    #if run out of guesses then start a new game
+    #if guess is wrong and remaining guesses = 0
+    elif guess != secret_number and guesses == 0:
+        print "Wrong guess!"
+        print "You have run out of guesses!"
+        print "Let's start a new game!"
+        print ""
+        #start a new game
+        new_game()
+        
+    elif guess > secret_number:
+        print "Secret number is lower!"
+        print ""
+        
+    elif guess < secret_number:
+        print "Secret number is higher!"
+        print ""
+        
     
     
 
