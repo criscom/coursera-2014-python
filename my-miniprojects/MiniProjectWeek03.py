899eeda145ebe570bf01f3b7a283668ac799d590
Christoph Potzinger <criscom27@gmail.com>
2014-10-11 20:26:23 +0200

    Finish timer

--- my-miniprojects/MiniProjectWeek03.py
+++ my-miniprojects/MiniProjectWeek03.py
@@ -5,37 +5,62 @@ import simplegui
 # define global variables
 
 #variable for incrementing the timer
-interval = 100
+t = 0
 
-t = "0"
+time = "0"
+
+#variable for the format
+
+#is timer running or not
+started = False
 
 #position of the timer on the canvas
-position = [150,150]
+position = [105,150]
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
-    pass
+    global time
+    # last digit or tenth of a seconds
+    D = t % 10
+    # second but last digit or seconds
+    C = ((t // 10) % 60) % 10
+    # third but last digit or tens of seconds
+    B = ((t // 10) % 60) // 10
+    A = t // 600
+    time = str(A)+":"+str(B)[0]+str(C)[0]+"."+str(D)
+    return time
+
     
 # define event handlers for buttons; "Start", "Stop", "Reset"
 def start_timer():
+    global started
     timer.start()
+    started = True
 
 def stop_timer():
+    global started
     timer.stop()
-    
+    started = False
+
+def reset_timer():
+    global t
+    global started
+    if started:
+       timer.stop()
+    t = 0
+       
 
 # define event handler for timer with 0.1 sec interval
 def timer_handler():
-    global interval
     global t
-    interval += 1
-    t = str(interval)
-    return t
+    t += 1
+
 
 # define draw handler
 def draw(canvas):
-    canvas.draw_text(t, position, 36, "#003465")
+    format(t)
+    canvas.draw_text(time, position, 36, "#003465")
     
 # create frame
 frame = simplegui.create_frame("Stopwatch: The Game", 300, 300, 200)
@@ -54,8 +79,11 @@ frame.add_button('Start timer', start_timer, 200)
 #create stop button
 frame.add_button('Stop timer', stop_timer, 200)
 
+#create reset button
+frame.add_button('Reset timer', reset_timer, 200)
+
 # create timer
-timer = simplegui.create_timer(interval, timer_handler)
+timer = simplegui.create_timer(100, timer_handler)
 
 # start frame
 frame.start()
