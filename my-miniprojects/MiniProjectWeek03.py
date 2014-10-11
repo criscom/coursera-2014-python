ef82fb721a668a496be017aab23a2d044a8d4d31
Christoph Potzinger <criscom27@gmail.com>
2014-10-11 21:11:45 +0200

    Finish MiniProject03

--- my-miniprojects/MiniProjectWeek03.py
+++ my-miniprojects/MiniProjectWeek03.py
@@ -7,27 +7,57 @@ import simplegui
 #variable for incrementing the timer
 t = 0
 
+#variable for the display of the stopwatch
 time = "0"
 
-#variable for the format
+#variable for counting the clicks on stop
+click = 0
+
+#variable for counting successful clicks
+success = 0
+
+#variable for displaying the score
+
+score = "0 / 0"
 
 #is timer running or not
 started = False
 
-#position of the timer on the canvas
-position = [105,150]
+#position for the timer on the canvas
+position_stopwatch = [105,150]
+
+#position for the score on the canvas
+position_score = [230,30]
 
 # define helper function format that converts time
 # in tenths of seconds into formatted string A:BC.D
 def format(t):
     global time
     # last digit or tenth of a seconds
+    # ex. 723 tenths of a second
+    # 723 % 10 = 3
+    # D = 3
     D = t % 10
+
     # second but last digit or seconds
+    # 723 // 10 = 72
+    # 72 % 60 = 12
+    # 12 % 10 = 2
+    # C = 1
     C = ((t // 10) % 60) % 10
+
     # third but last digit or tens of seconds
+    # 12 // 10 = 1
+    # B = 1
     B = ((t // 10) % 60) // 10
+    
+    # 723 // 600 = 1
+    # A = 1
     A = t // 600
+
+    # Stopp watch output as string
+    # t = A:BC.D
+    # 723 = 1:12.3
     time = str(A)+":"+str(B)[0]+str(C)[0]+"."+str(D)
     return time
 
@@ -39,13 +69,18 @@ def start_timer():
     started = True
 
 def stop_timer():
-    global started
+    global started, click, success, score
     timer.stop()
     started = False
+    click += 1
+    if (t % 10) == 0:
+        success +=1
+    score = str(success)+" / "+str(click)
+    return score
+    
 
 def reset_timer():
-    global t
-    global started
+    global t, started
     if started:
        timer.stop()
     t = 0
@@ -60,7 +95,9 @@ def timer_handler():
 # define draw handler
 def draw(canvas):
     format(t)
-    canvas.draw_text(time, position, 36, "#003465")
+    canvas.draw_text(time, position_stopwatch, 36, "#003465")
+    canvas.draw_text(score, position_score, 28, "#15cd30")
+
     
 # create frame
 frame = simplegui.create_frame("Stopwatch: The Game", 300, 300, 200)
