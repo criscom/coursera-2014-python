b48fa773a9ffd021066a09de06ff717e9709b1d9
Christoph Potzinger <criscom27@gmail.com>
2014-10-18 17:04:32 +0200

    Add paddles

--- my-miniprojects/MiniProjectWeek04.py
+++ my-miniprojects/MiniProjectWeek04.py
@@ -13,24 +13,35 @@ HALF_PAD_WIDTH = PAD_WIDTH / 2
 HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 LEFT = False
 RIGHT = True
-TIME = 0
 ball_pos = [WIDTH / 2, HEIGHT / 2]
-ball_vel = [0, 10] # pixels per update (1/60 seconds)
+ball_vel = [2, 4] # pixels per update (1/60 seconds)
+paddle1_pos = (HEIGHT / 2)
+paddle1_vel = 0
+paddle2_pos = (HEIGHT / 2)
+paddle2_vel = 0
+
 
 # initialize ball_pos and ball_vel for new bal in middle of table
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
     global ball_pos, ball_vel # these are vectors stored as lists
-
-
+    ball_pos = [WIDTH / 2, HEIGHT / 2]
+    if direction == RIGHT:
+        ball_vel[0] = random.randrange(2,-4)
+        ball_vel[1] = random.randrange(1,-3)
+    if direction == LEFT:
+        ball_vel[0] = random.randrange(2,4) * -1
+        ball_vel[1] = random.randrange(1,3) * -1
+        
 # define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2  # these are ints
+
    
     
 def draw(canvas):
-    global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
+    global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel, WIDTH, HALF_PAD_HEIGHT, HALF_PAD_WIDTH
  
         
     # draw mid line and gutters
@@ -44,7 +55,8 @@ def draw(canvas):
     ball_pos[1] += ball_vel[1]
     
 
-    
+    # 4. Modify your code such that the ball collides with and bounces
+    # off of the top and bottom walls.
     # let ball bounce from top and bottom walls
     if ball_pos[1] <= BALL_RADIUS:
         #top
@@ -54,20 +66,41 @@ def draw(canvas):
         ball_vel[1] = - ball_vel[1]
     
         
-    # draw ball
+    # 1. draw ball 
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "White", "#669fb2")
                        
     # update paddle's vertical position, keep paddle on the screen
     
+    ##############
     # draw paddles
+
+    # left paddle
+    #canva.draw_line((8/2, 200 - 40), (8/2, 200 + 40))
+    canvas.draw_line((HALF_PAD_WIDTH, paddle1_pos - HALF_PAD_HEIGHT), (HALF_PAD_WIDTH, paddle1_pos + HALF_PAD_HEIGHT), PAD_WIDTH, 'Red')
+    
+    # right paddle((600 - 8/2, 200 - 40)), (600 - 8/2, 200 + 40))
+    canvas.draw_line((WIDTH - HALF_PAD_WIDTH, paddle2_pos - HALF_PAD_HEIGHT), (WIDTH - HALF_PAD_WIDTH, paddle2_pos + HALF_PAD_HEIGHT), PAD_WIDTH, 'Blue')
+    
+    # END draw paddles
+    ###############
     
     # draw scores
         
 def keydown(key):
     global paddle1_vel, paddle2_vel
+    acc = 1
+    if key==simplegui.KEY_MAP["down"]:
+        paddle1_vel[1] += acc
+    if key==simplegui.KEY_MAP["up"]:
+        paddle1_vel[1] -= acc
    
 def keyup(key):
     global paddle1_vel, paddle2_vel
+    acc = 1
+    if key==simplegui.KEY_MAP["s"]: # move paddle down
+        paddle2_vel[1] += acc
+    if key==simplegui.KEY_MAP["w"]: # move paddle up
+        paddle2_vel[1] -= acc
 
     
 # register event handlers
